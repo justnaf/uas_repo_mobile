3aff6e110811400cf734f3bf756c3e85b88d95d8
Naufal <naufal86@gmail.com>
2022-07-04 00:47:43 +0700

    Welcome Edit

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:footer/footer.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'login_page.dart';
